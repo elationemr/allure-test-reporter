c0f87d2f16991ead8de6ca54e2ae387d679d5a00
John Ro <john.ro@elationhealth.com>
2022-01-14 15:37:04 -0800

    RM-25566 remove cluster_name from outputs

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -6,9 +6,9 @@ output "task_definition_arn" {
   value = aws_ecs_task_definition.service.arn
 }
 
-output "cluster_name" {
-  value = aws_ecs_service.ecs_service.cluster
-}
+# output "cluster_name" {
+#   value = aws_ecs_service.ecs_service.cluster
+# }
 
 # output "service_name" {
 #   value = aws_ecs_service.ecs_service.name
