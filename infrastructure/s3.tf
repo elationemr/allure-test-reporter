7005d66731dd6fc324fa0ce9db5106ba9ba76a4d
John Ro <john.ro@elationhealth.com>
2022-01-18 10:13:07 -0800

    RM-25566 add s3

---
+++ infrastructure/s3.tf
@@ -0,0 +1,16 @@
+module "test-data-bucket" {
+  source = "git::git@github.com:elationemr/terraform-modules.git//s3"
+
+  project       = local.project
+  environment   = local.workspace.environment
+  bucket_suffix = "test-data-bucket"
+
+  acl            = "public-read"
+  use_encryption = false
+
+  use_versioning = false
+
+  providers = {
+    aws = local.region
+  }
+}
