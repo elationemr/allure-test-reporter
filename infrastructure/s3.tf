5912bbb5ce6a624f26eb85e7ccead2428b8cad20
John Ro <john.ro@elationhealth.com>
2022-01-19 13:11:10 -0800

    RM-25566 tf fmt

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -2,12 +2,12 @@ resource "aws_s3_bucket" "test_data_bucket" {
   bucket        = "${local.project}-${local.workspace.environment}-test-data-bucket"
   acl           = "public-read"
   force_destroy = false
-#   server_side_encryption_configuration {
-#     rule {
-#       apply_server_side_encryption_by_default {
-#         sse_algorithm = "AES256"
-#       }
-#     }
-#   }
+  #   server_side_encryption_configuration {
+  #     rule {
+  #       apply_server_side_encryption_by_default {
+  #         sse_algorithm = "AES256"
+  #       }
+  #     }
+  #   }
   tags = local.default_tags
 }
