c28612d5472f0ff024be2aec2977630ebcbb52f0
John Ro <john.ro@elationhealth.com>
2022-01-19 17:53:52 -0800

    RM-25566 fix bucket name in script

--- scripts/generate-test-reports.sh
+++ scripts/generate-test-reports.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 aws sts get-caller-identity
-aws s3 cp s3://el8-dev-test-data-bucket/test-results test-results --recursive
+aws s3 cp s3://gp-dev-test-data-bucket/test-results test-results --recursive
 allure generate test-results -o test-report
