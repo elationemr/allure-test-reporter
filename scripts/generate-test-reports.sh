9865506af8e4d71d59aba353c0b18d785d63e97e
John Ro <john.ro@elationhealth.com>
2022-01-19 18:42:27 -0800

    RM-25566 copy test-report to s3

--- scripts/generate-test-reports.sh
+++ scripts/generate-test-reports.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-aws sts get-caller-identity
 aws s3 cp s3://gp-dev-test-data-bucket/test-results test-results --recursive
 allure generate test-results -o test-report
-ls -l test-report
+aws s3 cp --recursive ./test-report s3://gp-dev-test-data-bucket/test-report
